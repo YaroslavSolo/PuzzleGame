7420be41bc797f7d949a85d7d6b74c336ad50790
greencutie <dkapur@edu.hse.ru>
2021-10-04 23:12:41 +0300

    Totaltime add

--- PuzzleGame/ConsoleApp1/Program.cs
+++ PuzzleGame/ConsoleApp1/Program.cs
@@ -25,11 +25,14 @@ namespace ConsoleApp1
                     sr = "../../../result.csv";
                 }
                 List<DataReader.Probe> probes = DataReader.ReadAndParse(s);
+                long timetotal = 0;
                 for (int i = 0; i < probes.Count; ++i)
                 {
                     //Console.WriteLine("Test number " + i + "\n" + probes[i].ToString() + "\n\n");
                     FileReading.FileStreams.writeToFile(sr, "" + (i+1) + "," + probes[i].ToStringCSV() + "\n");
+                    timetotal += probes[i].totaltime;
                 }
+                FileReading.FileStreams.writeToFile(sr, "" + "Total time:," + timetotal + "\n");
             }
             catch(Exception e)
             {
